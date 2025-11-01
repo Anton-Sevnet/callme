0a6790086aa205dbb6c31912b7bb755cc03f6d1a
Anton Paschenko <info@alttechno.ru>
2025-11-01 18:28:21 +0300

    disable full log

--- CallMeIn.php
+++ CallMeIn.php
@@ -1185,7 +1185,7 @@ $pamiClient->registerEventListener(
         file_put_contents(getcwd() . '/logs/full.log', $log, FILE_APPEND);
     },function (EventMessage $event) use ($globalsObj) {
 
-    return true;
+    return false;
 }
 );
 
